b039a7854cc607c942c6119045ea3f4559799bf7
Andrew <ghobrial.andrew@gmail.com>
2023-05-18 05:09:43 -0400

    fixed internal server error

--- server.ts
+++ server.ts
@@ -47,19 +47,19 @@ function uriToPostLink(uri: string, usePsky: boolean) {
   }`;
 }
 function genTitle(author: ProfileViewDetailed, feed: FeedViewPost) {
-  const { handle } = author;
+  const { handle, displayName, avatar } = author;
   const { post, reason, reply } = feed;
   if (reason && reason["$type"] === BSKY_TYPES.repost) {
-    return `Repost by ${handle}, original by ${post.author.handle}`;
+    return `Repost by ${displayName} (${handle}), original by  ${post.author.displayName} (${post.author.handle})`;
   }
-  let title = `Post by ${handle}`;
+  let title = `${displayName}:`;
   if (reply) {
-    title = `${title}, reply to ${
+    title = `${title}, reply to @${
       actors[getDidFromUri(reply.parent.uri)].handle
     }`;
   }
   if (post.embed && post.embed["$type"] === BSKY_TYPES.view) {
-    title = `${title}, quoting ${post.embed.record!.author.handle}`;
+    title = `${title}, quoting ${post.embed.record!.author?.handle}`;
   } else if (post.embed && post.embed["$type"] === BSKY_TYPES.recordWithMedia) {
     title = `${title}, quoting ${post.embed.record!.record!.author.handle}`;
   }
@@ -93,7 +93,7 @@ function genMainContent(
     ),
     tag("p", sanitize(post.record.text).replace(/\n/, "<br>")),
     (post.embed && post.embed["$type"] === BSKY_TYPES.view)
-      ? tag("p", "<br>[quote]<br>", sanitize(post.embed.record!.value.text))
+      ? tag("p", "<br>[quote]<br>", sanitize(post.embed.record!.value?.text))
       : "",
     "]]>",
   ];
@@ -146,7 +146,7 @@ serve(async (request: Request) => {
     });
   }
 
-  const { did, handle } = await getActor(pathname.replace(/^\//, ""));
+  const { did, handle, displayName, avatar } = await getActor(pathname.replace(/^\//, ""));
   if (did === "") {
     return new Response("Unable to resolve handle", {
       headers: { "content-type": "text/plain" },
@@ -197,12 +197,12 @@ serve(async (request: Request) => {
         sanitize(href)
       }" rel="self" type="application/rss+xml" />`,
       tag("link", `https://staging.bsky.app/profile/${did}`),
-      tag("description", `${handle}'s posts in ${service}`),
+      tag("description", `${displayName} (${handle}) in ${service}`),
       tag("lastBuildDate", feeds.at(0)?.post.record.createdAt || ""),
       ...feeds.map(({ post, reason, reply }) =>
         tag(
           "item",
-          tag("title", genTitle({ did, handle }, { post, reason, reply })),
+          tag("title", genTitle({ did, handle, displayName }, { post, reason, reply })),
           tag("description", ...genMainContent(post, usePsky, includeRepost)),
           ...(post.embed?.images || []).map((image) =>
             `<enclosure type="image/jpeg" length="0" url="${image.thumb}"/>`
